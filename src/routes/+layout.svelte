bcc06a9d205864ba563e92278cb8ecb3f688b491
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-04 21:56:09 +0000

    mdsvex works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-    import { base } from '$app/paths';
-    console.log({base});
+	import { base } from '$app/paths';
 </script>
- 
+
 <nav>
-    <a href="{base}/">Home</a>
-    <a href="{base}/whoami">Whoami</a>
-    <a href="{base}/blog">Blog</a>
+	<a href="{base}/">Home</a>
+	<a href="{base}/whoami">Whoami</a>
+	<a href="{base}/blog">Blog</a>
 </nav>
 
 <slot></slot>
-
-
