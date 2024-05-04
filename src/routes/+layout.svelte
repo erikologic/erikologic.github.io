79d18c4df13e2d6ba3bd33d978283b39beb0c4a5
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-04 21:26:06 +0000

    support gh pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+    console.log({base});
+</script>
+ 
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</nav>
+
+<slot></slot>
+
+
