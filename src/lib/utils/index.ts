89cd0096d3aa085f81eabbb8f1e0e765f64ab93b
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-06 14:28:21 +0000

    Moved content location

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,11 +1,11 @@
 export const fetchMarkdownPosts = async () => {
-	const allPostFiles = import.meta.glob('/src/routes/blog/*.md');
+	const allPostFiles = import.meta.glob('/content/blog/*.md');
 	const iterablePostFiles = Object.entries(allPostFiles);
 
 	const allPosts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
 			const { metadata } = await resolver();
-			const postPath = path.slice(11, -3);
+			const postPath = path.slice(8, -3);
 
 			return {
 				meta: metadata,
