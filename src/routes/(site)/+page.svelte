268d012f234045990cc34c520b1bbe74a5ed5a27
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-08 21:51:03 +0000

    changed landing page message

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hey there 👋👋👋</h1>
+<p>Trying to do some cool with SvelteKit around here...</p>
