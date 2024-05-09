691438b96ceb2f887d6627fbc4ed72e91fdb287c
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-09 22:23:44 +0000

    add head for blog posts - need to do elsewhere

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+const config = {
+	title: 'Notes of a software t(h)inker',
+	description: 'Enrico Graziani - Product Engineer',
+	domain: 'https://erikologic.github.io/'
+};
+
+export default config;
--- src/routes/(site)/blog/[post]/+page.svelte
+++ src/routes/(site)/blog/[post]/+page.svelte
@@ -1,6 +1,13 @@
 <script>
+	import config from '$lib/config';
+
 	export let data;
 </script>
+
+<svelte:head>
+	<title>{data.meta.title} - {config.title}</title>
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
 
 <article>
 	<h1>{data.meta.title}</h1>
