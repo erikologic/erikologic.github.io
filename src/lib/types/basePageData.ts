b8551b1727b20dfb623e849c07b9443386f13661
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-10 23:00:54 +0000

    add basePageData

---
+++ src/lib/types/basePageData.ts
@@ -0,0 +1,15 @@
+export interface BaseMetadata {
+	title: string;
+	description: string;
+	type?: string;
+	
+	image?: {
+		url: string;
+		width: string;
+		height: string;
+	};
+}
+
+export interface BasePageData {
+	meta: BaseMetadata;
+}
--- src/routes/(site)/+page.ts
+++ src/routes/(site)/+page.ts
@@ -1,5 +1,11 @@
-export const load = async () => {
+import type { BasePageData } from '$lib/types/basePageData';
+
+// TODO use in other places
+export const load = async (): Promise<BasePageData> => {
 	return {
-		meta: { title: '/' }
+		meta: {
+			title: '/',
+			description: 'Home page'
+		}
 	};
 };
