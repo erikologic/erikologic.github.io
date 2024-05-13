0730ca65226583fb6ddee7c4dc5cae7e9948f941
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-13 20:57:02 +0000

    refactoring posts

--- src/lib/assets/js/utils/posts.ts
+++ src/lib/assets/js/utils/posts.ts
@@ -1,35 +1,39 @@
 // TODO this should come from the list of articles!
+// It is safe to store things in memory here because we are building for a static site
 const posts = [
 	{
 		slug: 'hello-world',
 		title: 'Hello World',
 		description: 'This is the first post on this blog',
 		publishedTime: '2021-01-01',
+		modifiedTime: '2021-01-01',
 		tags: ['personal']
+	},
+	{
+		slug: 'foo-bar',
+		title: 'Foo Bar',
+		description: 'This is a foo bar',
+		publishedTime: '2019-01-01',
+		modifiedTime: '2019-01-01',
+		tags: ['foo-bar']
 	}
 ];
+
+const tags = new Set(posts.flatMap((p) => p.tags));
+
 const defaultOptions = {
 	tags: 'all',
-	page: 1
+	page: 1,
+	pageSize: 10
 };
 
-export const getPosts = async ({ tags, page } = defaultOptions) => {
-	return posts;
+export const getPosts = async ({ tags, page, pageSize } = defaultOptions) => {
+	return posts.slice((page - 1) * pageSize, page * pageSize);
 };
 
-// TODO this should come from an actual article!
-const helloWorldPost = {
-	slug: 'hello-world',
-	title: 'Hello World',
-	description: 'This is the first post on this blog',
-	publishedTime: '2021-01-01',
-	modifiedTime: '2021-01-01',
-	tags: ['personal']
-};
+interface GetPostOptions {
+	post: string;
+}
+export const getPost = async ({ post }: GetPostOptions) => posts.find((p) => p.slug === post);
 
-export const getPost = async ({ post }) => helloWorldPost;
-
-
-// TODO this should come from the list of articles!
-const tags = ['personal', 'tech', 'food', 'travel'];
 export const getTags = async () => tags;
