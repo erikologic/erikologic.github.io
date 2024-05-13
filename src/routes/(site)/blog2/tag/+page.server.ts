8da6e8b7de4386b36ca5cd0f68d41003856ed036
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-13 21:50:07 +0000

    counting tags

--- src/routes/(site)/blog2/tag/+page.server.ts
+++ src/routes/(site)/blog2/tag/+page.server.ts
@@ -4,7 +4,7 @@ import { getTags } from '$lib/assets/js/utils/posts';
 
 // export const load = async ({ params }): Promise<BasePageData> => {
 export const load = async () => {
-	const tags = await getTags();
+	const tags = await getTags({});
 	return {
 		meta: {
 			title: `Blog - Tags`,
