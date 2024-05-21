720e1296e9c7665b043de1b21b8546836a7efa4b
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-21 22:37:48 +0000

    installed daisyui

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -4,9 +4,3 @@
 	Not the best sight is it?!<br />
 	Well, I have been procrastinating this for years so let's just ship something and iterate...<br />
 </p>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.200);
-	}
-</style>
