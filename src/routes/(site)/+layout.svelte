8f34f6fad7e9e5c436a357d204df9f1dbf43f6a8
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-21 22:36:42 +0000

    installed tailwind

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../../app.css';
 	import { base } from '$app/paths';
 	import { fade } from 'svelte/transition';
 	import { page } from '$app/stores';
--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,6 +1,12 @@
 <h1>Hey there 👋👋👋</h1>
 
 <p>
-    Not the best sight is it?!<br/>
-    Well, I have been procrastinating this for years so let's just ship something and iterate...<br/>
+	Not the best sight is it?!<br />
+	Well, I have been procrastinating this for years so let's just ship something and iterate...<br />
 </p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.200);
+	}
+</style>
