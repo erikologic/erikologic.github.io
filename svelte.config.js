0ca68e2c5dd9bffe929eae36026d1f3787defa86
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-05-21 21:34:14 +0000

    switched to svelte preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkToc from 'remark-toc';
-import autoprefixer
-	from 'autoprefixer';
+import autoprefixer from 'autoprefixer';
+import sveltePreprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess({
-			postcss: { plugin: [autoprefixer] }
+		sveltePreprocess({
+			postcss: {
+				plugins: [autoprefixer()],
+			},
 		}),
 		mdsvex({
 			extensions: ['.md'],
