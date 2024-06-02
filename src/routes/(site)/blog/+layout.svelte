bbc3b6912d8446b1b8f9d3fe5945563435117dd9
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-06-02 22:40:46 +0000

    prevented content jumping

--- src/routes/(site)/blog/+layout.svelte
+++ src/routes/(site)/blog/+layout.svelte
@@ -7,25 +7,27 @@
 <!-- TODO Breadcrumbs -->
 <!-- <h1>Blog -> Tag: ALL -> Page 1</h1> -->
 
-<aside class="prose mb-12 flex-1 px-8">
-	<h2>Most recent posts</h2>
-	<ul>
-		{#each data.recentPosts as post}
-			<li>
-				<a href="{base}/blog/post/{post.slug}">{post.title}</a>
-			</li>
-		{/each}
-	</ul>
-	<h3><a href="{base}/blog/tag">Tags</a></h3>
-	<ul>
-		{#each data.tags as tag}
-			<li>
-				<a href="{base}/blog/tag/{tag}/1">{tag}</a>
-			</li>
-		{/each}
-	</ul>
-</aside>
+<div class="flex flex-col lg:flex-row">
+	<aside class="prose mb-12 px-8 lg:w-72">
+		<h2>Most recent posts</h2>
+		<ul>
+			{#each data.recentPosts as post}
+				<li>
+					<a href="{base}/blog/post/{post.slug}">{post.title}</a>
+				</li>
+			{/each}
+		</ul>
+		<h3><a href="{base}/blog/tag">Tags</a></h3>
+		<ul>
+			{#each data.tags as tag}
+				<li>
+					<a href="{base}/blog/tag/{tag}/1">{tag}</a>
+				</li>
+			{/each}
+		</ul>
+	</aside>
 
-<main class="prose flex-1 px-8">
-	<slot />
-</main>
+	<main class="prose px-8">
+		<slot />
+	</main>
+</div>
